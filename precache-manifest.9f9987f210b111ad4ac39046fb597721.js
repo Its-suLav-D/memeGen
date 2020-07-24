420abaac8a6e4ae581a9a3fd04e738aa2da7d8c1
Its-suLav-D <dah19001@byui.edu>
2020-07-24 15:08:23 -0700

    Updates

---
+++ precache-manifest.9f9987f210b111ad4ac39046fb597721.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "512837e0ae379a2150d0003487e251e4",
+    "url": "/memeGen/index.html"
+  },
+  {
+    "revision": "742a2bd68fb14168d49d",
+    "url": "/memeGen/static/css/main.f2c68b56.chunk.css"
+  },
+  {
+    "revision": "4831f0b1bfc17b3266e8",
+    "url": "/memeGen/static/js/2.02143386.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memeGen/static/js/2.02143386.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "742a2bd68fb14168d49d",
+    "url": "/memeGen/static/js/main.5408c56a.chunk.js"
+  },
+  {
+    "revision": "bfbd1f0e982d87a03e3a",
+    "url": "/memeGen/static/js/runtime-main.b2c86481.js"
+  }
+]);
